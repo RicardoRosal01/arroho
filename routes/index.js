daf083125aa3e0da545b13ce828ceb32c24fc310
Ricardo Rosal <crosalri@onjci.com>
2020-06-08 09:53:30 -0300

    alterações da reunião 02/06

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,29 @@ const express = require('express')
 const Router = express.Router()
 
 Router.get('/',(req,res)=>{
-    res.render('index')
+    res.render('clave')
+})
+
+Router.post('/',(req,res)=>{
+    let mensagem = 'clave incorreta'
+    if (req.body.clave == "XMG3-Rel.1") {
+        res.render('index')
+    } else {
+        res.render('clave',{mensagem:mensagem})
+    }
 })
 
 Router.get('/carga',(req,res)=>{
-    res.render('cargaTermica')
+    res.render('claveCarga')
+})
+
+Router.post('/carga',(req,res)=>{
+    let mensagem = 'clave incorreta'
+    if (req.body.clave == "XMG3-Rel.1") {
+        res.render('carga')
+    } else {
+        res.render('clave',{mensagem:mensagem})
+    }
 })
 
 module.exports = {Router}
